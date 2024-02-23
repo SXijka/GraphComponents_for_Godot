652a57587ea9edbe696af102b9935eaf25a82fde
SXijka <gxieja@proton.me>
2024-02-23 18:41:27 +0800

    对文本节点进行修改，“文本节点”类现在继承自“节点”类，属于“文本节点”和“节点”组

--- 组件/文本节点/文本节点.gd
+++ 组件/文本节点/文本节点.gd
@@ -1,5 +1,6 @@
-extends Control
+@tool
 class_name 文本节点
+extends 节点
 
 @export var 自动展现: bool = true
 @export var 展开大小: Vector2 = Vector2(300, 100)
@@ -7,22 +8,22 @@ class_name 文本节点
 
 @export_range(1.0, 10.0, 0.1) var 展开时间: float = 3
 @export var 内容: String = "……"
-@export var 名称: String = "文本"
-@export var 颜色: Color = Color.ROYAL_BLUE
 @export_file("*.txt") var 内容文件: String
 
-@onready var 节点本体: 节点 = $"节点"
-@onready var 文本框: ScrollContainer = 节点本体.获取_内容()[0]
-@onready var 文本: RichTextLabel = 节点本体.获取_内容()[0].get_child(0)
+
+@onready var 文本框: ScrollContainer = $"可调整边框/节点内容/面板背景/面板容器/面板/滚动框"
+@onready var 文本: RichTextLabel = $"可调整边框/节点内容/面板背景/面板容器/面板/滚动框/富文本"
 
 
 func _init() -> void:
+	super._init()
 	add_to_group("文本节点")
 
 
 func _ready() -> void:
-	节点本体.名称 = 名称
-	节点本体.标记颜色 = 颜色
+	super._ready()
+	if Engine.is_editor_hint():
+		return
 	if 内容文件:
 		文本.text = FileAccess.open(内容文件, FileAccess.READ).get_as_text()
 	else:
@@ -39,11 +40,10 @@ func 隐藏():
 
 func 展现():
 	show()
-	节点本体.节点大小 = 隐藏大小
+	节点大小 = 隐藏大小
 
 	var 展开动画 = 文本.create_tween()
 	展开动画.set_trans(Tween.TRANS_CUBIC)
-	展开动画.tween_property(节点本体, "节点大小",Vector2(展开大小.x, 节点本体.size.y) ,展开时间/3)
-	展开动画.tween_property(节点本体, "节点大小", 展开大小, 展开时间 * 2 / 3)
+	展开动画.tween_property(self, "节点大小",Vector2(展开大小.x, size.y) ,展开时间/3)
+	展开动画.tween_property(self, "节点大小", 展开大小, 展开时间 * 2 / 3)
 	展开动画.parallel().tween_property(文本, "visible_ratio", 1.0, 展开时间 * 2 / 3)
-
